d758717a958760ce4b80a07f6e2d3c57778b9faa
Rafal Jeczalik <rjeczalik@gmail.com>
2017-12-05 23:30:13 +0100

    sol-rope: rework to use GET endpoint

--- rope.sol
+++ rope.sol
@@ -20,15 +20,35 @@ contract usingRope is usingOraclize {
 		return rope_raw(strConcat("https://run.rope.live/", method));
 	}
 
-	function rope_run(string method, string json) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", method), json);
+	function rope_run(string method, string arg) internal returns (bytes32 _id) {
+		return rope_raw(strConcat("https://run.rope.live/", method, "?0=", arg));
+	}
+
+	function rope_run(string method, string arg1, string arg2) internal returns (bytes32 _id) {
+		return rope_raw(strConcat("https://run.rope.live/", method, "?0=", arg1, "&1=", arg2));
+	}
+
+	function rope_run(string method, string arg1, string arg2, string arg3) internal returns (bytes32 _id) {
+		return rope_raw(strConcat("https://run.rope.live/", method, "?0=", arg1, "&1=", arg2, "&2=", arg3));
 	}
 
 	function rope_run_id(string id, string method) internal returns (bytes32 _id) {
 		return rope_raw(strConcat("https://run.rope.live/", id, "/", method));
 	}
 
-	function rope_run_id(string id, string method, string json) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", id, "/", method), json);
+	function rope_run_id(string id, string method, string arg) internal returns (bytes32 _id) {
+		string args = strConcat("?0=", arg);
+		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
 	}
+
+	function rope_run_id(string id, string method, string arg1, string arg2) internal returns (bytes32 _id) {
+		string args = strConcat("?0=", arg1, "&1=", arg2);
+		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
+	}
+
+	function rope_run_id(string id, string method, string arg1, string arg2, string arg3) internal returns (bytes32 _id) {
+		string args = strConcat("?0=", arg1, "&1=", arg2, "&2=", arg3);
+		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
+	}
+
 }
