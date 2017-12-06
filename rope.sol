b5f48b19953608c0a492844a8c6df4da9b64e451
Rafal Jeczalik <rjeczalik@gmail.com>
2017-12-06 22:02:44 +0100

    sol-rope: update rope api

--- rope.sol
+++ rope.sol
@@ -17,38 +17,38 @@ contract usingRope is usingOraclize {
 	}
 
 	function rope_run(string method) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", method));
+		return rope_raw(strConcat("https://run.rope.live/method/", method));
 	}
 
 	function rope_run(string method, string arg) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", method, "?args=", arg));
+		return rope_raw(strConcat("https://run.rope.live/method/", method, "?args=", arg));
 	}
 
 	function rope_run(string method, string arg1, string arg2) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", method, "?args[0]=", arg1, "&args[1]=", arg2));
+		return rope_raw(strConcat("https://run.rope.live/method/", method, "?args[0]=", arg1, "&args[1]=", arg2));
 	}
 
 	function rope_run(string method, string arg1, string arg2, string arg3) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", method, "?args[0]=", arg1, "&args[1]=", arg2, "&args[2]=", arg3));
+		return rope_raw(strConcat("https://run.rope.live/method/", method, "?args[0]=", arg1, "&args[1]=", arg2, "&args[2]=", arg3));
 	}
 
 	function rope_run_id(string id, string method) internal returns (bytes32 _id) {
-		return rope_raw(strConcat("https://run.rope.live/", id, "/", method));
+		return rope_raw(strConcat("https://run.rope.live/method/", id, "/", method));
 	}
 
 	function rope_run_id(string id, string method, string arg) internal returns (bytes32 _id) {
 		string args = strConcat("?args[0]=", arg);
-		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
+		return rope_raw(strConcat("https://run.rope.live/method/", id, "/", method, args));
 	}
 
 	function rope_run_id(string id, string method, string arg1, string arg2) internal returns (bytes32 _id) {
 		string args = strConcat("?args[0]=", arg1, "&args[1]=", arg2);
-		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
+		return rope_raw(strConcat("https://run.rope.live/method/", id, "/", method, args));
 	}
 
 	function rope_run_id(string id, string method, string arg1, string arg2, string arg3) internal returns (bytes32 _id) {
 		string args = strConcat("?args[0]=", arg1, "&args[1]=", arg2, "&args[2]=", arg3);
-		return rope_raw(strConcat("https://run.rope.live/", id, "/", method, args));
+		return rope_raw(strConcat("https://run.rope.live/method/", id, "/", method, args));
 	}
 
 }
